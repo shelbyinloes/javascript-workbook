891ad2e92549170c003bd9f92633c6e10523df47
Shelby Inloes <shelbyinloes@gmail.com>
2018-02-06 19:23:22 -0600

    new branch

---
+++ 04week/gitOlympics2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const printListOfOlympians = (array) => {
+  array.split().forEach((array) => {
+    console.log(array);
+  })
+}
+
+printListOfOlympians("Shawn White, Gus Kenworthy, Simone Biles, Shelby Inloes, Justin Oswald");
