cb43373e0c1af6930d1824c486c79bcbc17c42ee
Shelby Inloes <shelbyinloes@gmail.com>
2018-02-13 19:43:48 -0600

    second function

--- 04week/loops-practice.js
+++ 04week/loops-practice.js
@@ -55,6 +55,12 @@ baseAndDate();
 
 //create a function that returns a list of availble currencies from a given object. Use latestExchangeRate as an example to test your function
 
+const availableCurrencies = () => {
+    return Object.keys(latestExchangeRate.rates);
+}
+
+availableCurrencies();
+
 // create a function that console logs a string with the currency and the currency rate for each currency in a given object. For example, 'AUD is at a 1.2742 conversion rate'. Use latestExchangeRate as an example to test your function
 
 //create a function that takes in an object and console logs the result of the object's multiplyByTen method on each of the exchange rates. Use latestExchangeRate as an example to test your function
