754a905840b6dea6e3f689f6a5cf3066cd386d6a
Shelby Inloes <shelbyinloes@gmail.com>
2018-03-03 10:33:07 -0600

    created new componenets

--- 07week/react-tictactoe/src/App.js
+++ 07week/react-tictactoe/src/App.js
@@ -1,41 +1,30 @@
 import React, { Component } from 'react';
+import Board from './Board.js';
 import logo from './logo.svg';
 import './App.css';
 
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      board: [
+        [0, 1, 2],
+        [0, 1, 2],
+        [0, 1, 2]
+      ],
+      playerTurn: 0, 
+      win: 0, 
+    }
   }
 
-  //board = [
-  //   [0, 1, 2], 
-  //   [3, 4, 5],
-  //   [6, 7, 8],
-  // ];
-
-  // board[2][0] is same as spot 6 
-
-  //const [ zero, one, two, ...board] = this.state.board
+  //functions go here
 
   render() {
     return (
       <div>
-        <div className="row">
-          <div data-cell="0">0</div>
-          <div data-cell="1">0</div>
-          <div data-cell="2">0</div>
-        </div>
-        <div className="row">
-          <div data-cell="3">0</div>
-          <div data-cell="4">0</div>
-          <div data-cell="5">0</div>
-        </div>
-        <div className="row">
-          <div data-cell="6">0</div>
-          <div data-cell="7">0</div>
-          <div data-cell="8">0</div>
-        </div>
+        board.map((row, i) => {
+          <board Row/>
+        })
       </div>
     );
   }
