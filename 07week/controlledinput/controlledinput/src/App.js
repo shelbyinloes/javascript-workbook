4c9ccddd595ef90ad25ae1d20a2db0803b529daf
Shelby Inloes <shelbyinloes@gmail.com>
2018-03-06 19:24:16 -0600

    start of project & first function

--- 07week/controlledinput/controlledinput/src/App.js
+++ 07week/controlledinput/controlledinput/src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+  }
+
+  handleInputValue(e){
+    //this.setState
+    //needs to be fat arrow
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input></input><button onClick={this.handleInputValue}>Submit</button>
       </div>
     );
   }
