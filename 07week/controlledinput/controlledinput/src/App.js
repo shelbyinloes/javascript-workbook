a06f7cb6c4dba3227360e30dcffdf940db66f0ef
Shelby Inloes <shelbyinloes@gmail.com>
2018-03-08 21:01:05 -0600

    material ui

--- 07week/controlledinput/controlledinput/src/App.js
+++ 07week/controlledinput/controlledinput/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 
 class App extends Component {
@@ -8,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: ''
+      list: []
     }
   }
 
@@ -17,18 +20,33 @@ class App extends Component {
     //needs to be fat arrow
   }
   
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input></input><button onClick={this.handleInputValue}>Submit</button>
+
+  render() { 
+
+    const style = {
+      margin: 12,
+    };
+    
+    const RaisedButtonExampleSimple = () => (
+      <div>
+        <RaisedButton label="Submit" primary={true} style={style} />
       </div>
+    );
+
+    return (
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <input></input><button onClick={this.handleInputValue}>Submit</button>
+          {RaisedButtonExampleSimple()}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
