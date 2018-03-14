55fb7d94858beaba80381b508f2f839ef7d53999
Shelby Inloes <shelbyinloes@gmail.com>
2018-03-13 21:37:45 -0500

    working, need to refactor a lot

--- 07week/newTicTacToe/src/App.js
+++ 07week/newTicTacToe/src/App.js
@@ -15,13 +15,69 @@ class App extends Component {
         ' ', ' ', ' ',
         ' ', ' ', ' '
       ], 
-      turn: 'X', 
+      turn: 'x', 
       winner: null
     }
   }
 
   updateBoard(loc, player) {
+    if(this.state.gameBoard[loc] === 'x' || this.state.gameBoard[loc] === 'o' || this.state.winner){
+      //invalid move
+      return;
+    }
+    const currentGameBoard = this.state.gameBoard;
+    currentGameBoard.splice(loc, 1, this.state.turn);
+    this.setState({gameBoard: currentGameBoard});
 
+    const topRow = this.state.gameBoard[0] + this.state.gameBoard[1] + this.state.gameBoard[2];
+    if(topRow.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+    const middleRow = this.state.gameBoard[3] + this.state.gameBoard[4] + this.state.gameBoard[5];
+    if(middleRow.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+    const bottomRow = this.state.gameBoard[6] + this.state.gameBoard[7] + this.state.gameBoard[8];
+    if(bottomRow.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+
+    const leftCol = this.state.gameBoard[0] + this.state.gameBoard[3] + this.state.gameBoard[6];
+    if(leftCol.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+    const middleCol = this.state.gameBoard[1] + this.state.gameBoard[4] + this.state.gameBoard[7];
+    if(middleCol.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+    const rightCol = this.state.gameBoard[2] + this.state.gameBoard[5] + this.state.gameBoard[8];
+    if(rightCol.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+
+    const rightDiag = this.state.gameBoard[0] + this.state.gameBoard[4] + this.state.gameBoard[8];
+    if(rightDiag.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+    const leftDiag = this.state.gameBoard[2] + this.state.gameBoard[4] + this.state.gameBoard[6];
+    if(leftDiag.match(/xxx|ooo/)) {
+      this.setState({winner: this.state.turn});
+      return;
+    }
+
+    const moves = this.state.gameBoard.join('').replace(/ /g, '');
+    if(moves.length === 9) {
+      this.setState({winner: 'draw'});
+    }
+
+    this.setState({turn: (this.state.turn === 'x') ? 'o' : 'x'})
   }
 
   resetBoard() {
@@ -31,7 +87,7 @@ class App extends Component {
         ' ', ' ', ' ',
         ' ', ' ', ' '
       ], 
-      turn: 'X', 
+      turn: 'x', 
       winner: null
     })
   }
@@ -48,7 +104,7 @@ class App extends Component {
             return(
               <Tile 
                 key={i}
-                Loc={i}
+                loc={i}
                 value={value}
                 updateBoard={this.updateBoard.bind(this)}
                 turn={this.state.turn}
